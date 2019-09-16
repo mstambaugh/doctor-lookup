ffe8fd52fce363377dfcf9ad5f0d3fd57b05219b
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-15 23:22:18 -0700

    tidy comments

--- src/main.js
+++ src/main.js
@@ -28,20 +28,5 @@ $(document).ready(function() {
     // };
 
 
-    // const thesWord = $('#word').val();
-    // // $('#word').val("");
-    // console.log(thesWord);
-    //
-    // let synonym = new Thesaurus();
-    // let promise2 = synonym.getSynonymByWord(thesWord)
-    //
-    // promise2.then(function(response) {
-    //   const body2 = JSON.parse(response);
-    //   console.log(body2[0].meta.syns[0]);
-    //     $('#result2').append(`<p>${thesWord} means ${body2[0].meta.syns[0].join(", ")}</p>`);
-    // }, function(error) {
-    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    // });
-
   });
 });
