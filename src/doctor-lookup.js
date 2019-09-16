a1b5126bca0a0e32a8746d3b38b7fe2f102a69c5
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-15 23:12:31 -0700

    add pathing for doctor information, html formatting, doctor lookup by name

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -2,7 +2,7 @@ export class Doctors {
 getDoctorbyAilment(ailment) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${ailment}&location=45.5051,-122.6750,100&limit=20&user_key=${process.env.exports.apiKey}`;
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${ailment}&location=45.5051,-122.6750,100&limit=20&user_key=${process.env.exports.apiKey}`;
       console.log(url);
       request.onload = function() {
         if (this.status === 200) {
@@ -16,22 +16,3 @@ getDoctorbyAilment(ailment) {
     });
   }
 }
-
-// export class Thesaurus {
-// getSynonymByWord(thesWord) {
-//     return new Promise(function(resolve, reject) {
-//       let request = new XMLHttpRequest();
-//       const url = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/${thesWord}?key=${process.env.OTHER_API_KEY}`;
-//       console.log(url);
-//       request.onload = function() {
-//         if (this.status === 200) {
-//           resolve(request.response);
-//         } else {
-//           reject(Error(request.statusText));
-//         }
-//       }
-//       request.open("GET", url, true);
-//       request.send();
-//     });
-//   }
-// }
--- src/main.js
+++ src/main.js
@@ -4,24 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 import { Doctors } from './../src/doctor-lookup.js';
-import {  } from './../src/doctor-lookup.js';
 
 $(document).ready(function() {
   $('#docSearchBtn').click(function() {
-    const ailment = $('#ailmentInput').val();
-
-    console.log(ailment);
+    const illness = $('#illnessInput').val();
+    const docName = $('#docNameInput').val();
+    console.log(docName);
 
 
     let docResults = new Doctors();
-    let promise1 = docResults.getDoctorbyAilment(ailment);
+    let promise1 = docResults.getDoctorbyAilment(docName, illness);
 
     promise1.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body);
+        for (let i=0; i<body.data.length; i++)
+        $('#doctorResults').append(`<p>Here are some recommended doctors in your area<br>${body.data[0].profile.first_name} ${body.data[0].profile.last_name}<br> ${body.data[0].practices[0].phones[0].number}<br>${body.data[0].practices[0].visit_address.street} ${body.data[0].practices[0].visit_address.city} ${body.data[0].practices[0].visit_address.state} ${body.data[0].practices[0].visit_address.zip}<br> ${body.data[0].practices[0].website}<br></p>`);
 
-        $('#doctorResults').append(`<p>You can see any of these  ${body[0]}</p>`);
       });
+
 
     //  function(error) {
     //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
