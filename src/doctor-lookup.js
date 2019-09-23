a52c21ffdcf97a437e0b70b7155f140c8fce75df
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-22 23:46:57 -0700

    add for loop for invalid entries, add empty string for new search load, change html display for doctor results

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,9 +1,8 @@
 export class Doctors {
-getDoctorsbyIllness(illness) {
+getDoctorsbyIllness(docName, illness) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${ailment}&location=45.5051,-122.6750,100&limit=20&user_key=${process.env.exports.apiKey}`;
-      console.log(url);
+      const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${illness}&location=45.5051,-122.6750,100&limit=20&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ $(document).ready(function() {
   $('#docSearchBtn').click(function() {
     const docName = $('#docNameInput').val();
     const illness = $('#illnessInput').val();
-
-
+  $('#doctorResults').text("");
 
     let docResults = new Doctors();
     let promise1 = docResults.getDoctorsbyIllness(docName, illness);
@@ -18,13 +17,13 @@ $(document).ready(function() {
     promise1.then(function(response) {
       const body = JSON.parse(response);
       if (body.meta.count>0) {
-        body.data.forEach (function(doctor) {
-          $('#doctorResults').append(`<p>Here are some recommended doctors in your area<br>${doctor.profile.first_name} ${doctor.profile.last_name}<br> ${doctor.practices[0].phones[0].number}<br>${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}<br> ${doctor.practices[0].website}<br></p>`)
-        }
+        $('#doctorResults').append(`<p>Here are some recommended doctors in your area<br>`);
+        body.data.forEach(function(doctor) {
+          $('#doctorResults').append(`${doctor.profile.first_name} ${doctor.profile.last_name}<br> ${doctor.practices[0].phones[0].number}<br>${doctor.practices[0].visit_address.street} ${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state} ${doctor.practices[0].visit_address.zip}<br> ${doctor.practices[0].website}<br></p>`)
+        });
       } else {
         $('#doctorResults').append(`<p> We couldn't find any doctors matching your search. Please try again with different search parameters</p>`);
       }
-
     });
   });
 });
